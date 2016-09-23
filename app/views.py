0b3c5397501bfbae03e5be9c2845c1e54db535c6
Andrew <Andrew@Andrews-MacBook-Pro-2.local>
2016-09-23 17:50:16 -0400

    posts

--- app/views.py
+++ app/views.py
@@ -4,7 +4,22 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'nickname': 'Miguel'}
+	user = {'nickname': 'Andrew'}
+	posts = [
+		{
+			'author': { 'nickname': 'Andrew' },
+			'body': 'Beautiful day at Domi!'
+		},
+		{
+			'author': { 'nickname': 'Carolyn' },
+			'body': 'Missing the Domi station!'
+		},
+                {
+                        'author': { 'nickname': 'William' },
+                        'body': 'Super excited about Proper Channel'
+                }
+	]
 	return render_template('index.html',
 				title='Home',
-				user=user) 
+				user=user,
+				posts=posts)
